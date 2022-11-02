52a30b3ce24f597397091ba697c0997dde60bb7f
Jakub Tomys <kuba_praktyki@outlook.com>
2022-11-02 12:11:32 +0100

    Add nav animations

--- BlazorWebAssembly/Program.cs
+++ BlazorWebAssembly/Program.cs
@@ -1,3 +1,4 @@
+using BlazorAnimation;
 using BlazorWebAssembly;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -9,5 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+
+builder.Services.Configure<AnimationOptions>(Guid.NewGuid().ToString(), c => { });
 
 await builder.Build().RunAsync();
