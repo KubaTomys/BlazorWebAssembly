f14d9d4edc96a2b36007f87faa7418e2e6beb3c3
Marcin Perz <marcin.perz@Nordan.pl>
2022-11-02 13:22:36 +0100

    Adds onSCroll service

---
+++ BlazorWebAssembly/Components/MyAnimationBase.cs
@@ -0,0 +1,52 @@
+﻿using BlazorWebAssembly.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using MudBlazor.Interop;
+
+namespace BlazorWebAssembly.Components
+{
+    public class MyAnimationBase : ComponentBase
+    {
+        [Inject]
+        public IScrollInfoService ScrollInfoService { get; set; } = default!;
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; } = default!;
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+
+        public bool Enabled { get; set; } = false;
+        public BoundingClientRect? BoundingClientRect { get; set; } = default;
+        protected ElementReference? ElementReference { get; set; }
+
+        protected override void OnInitialized()
+        {
+            EventHandler<int> eventHandler = new EventHandler<int>(async (s, e) => await ScrollInfoService_OnScroll(s, e));
+            ScrollInfoService.OnScroll += eventHandler;
+            base.OnInitialized();
+
+        }
+
+        private async Task<int> ScrollInfoService_OnScroll(object? sender, int e)
+        {
+
+            BoundingClientRect = await JSRuntime.InvokeAsync<BoundingClientRect>("MyDOMGetBoundingClientRect", new object[] { ElementReference });
+            if (BoundingClientRect == null)
+            {
+                return default;
+            }
+            var yValue = ScrollInfoService.YValue;
+            if (yValue > BoundingClientRect.Top +400 )
+            {
+                Enabled = true;
+            }
+            await InvokeAsync(() => StateHasChanged());
+            return default;
+        }
+        public void Dispose()
+        {
+            //ScrollInfoService.OnScroll -= ScrollInfoService_OnScroll;
+        }
+    }
+
+
+}
